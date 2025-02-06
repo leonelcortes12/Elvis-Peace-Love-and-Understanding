a1195dd2abd0f781fe219f6dc310109e6d330c08
leonelcortes12 <leocfender@gmail.com>
2025-02-06 10:50:11 -0800

    Initial Commit

---
+++ Elvis Peace Love and Understanding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis Peace Love and Understanding
+//
+//  Created by Student1 on 2/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis Peace Love and Understanding/Elvis_Peace_Love_and_UnderstandingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Elvis_Peace_Love_and_UnderstandingApp.swift
+//  Elvis Peace Love and Understanding
+//
+//  Created by Student1 on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Elvis_Peace_Love_and_UnderstandingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
